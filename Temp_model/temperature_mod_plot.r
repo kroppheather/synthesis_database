3cd451e57dfa29e361fb8e4d3aa2f1f571f63d6c
kroppheather <kroppheather@gmail.com>
2017-02-06 16:06:34 -0500

    added more paramter plots

--- Temp_model/temperature_mod_plot.r
+++ Temp_model/temperature_mod_plot.r
@@ -60,6 +60,7 @@ depthP<-list()
 colP<-c(terrain.colors(7),heat.colors(10),topo.colors(10))
 for(i in 1:dim(sitesS)[1]){
 	depthP[[i]]<-datSI$depth[datSI$siteid==sitesS$siteid[i]]
+	
 
 }
 #set up list of x variables for sine function
@@ -204,4 +205,116 @@ for(n in 1:dim(sitesS)[1]){
 			
 	text(min(datSM$decdateA[datSM$siteid==i])+.5,max(datSM$T[datSM$siteid==i])-.5, paste("siteid=", sitesS$siteid[i]), cex=2)
 	dev.off()
+}
+
+
+#read in some of the site and vegetation data
+
+#site vegetation class
+datVClass<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\vege_class.csv")
+#site mapped soil C
+datSoilC<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\soilc_30cm.csv")
+
+
+#match up soil and vege measurements
+colnames(datVClass)<-c("siteid","class")
+colnames(datSoilC)<-c("siteid", "soilC30")
+datSS1<-join(datSS,datVClass,by="siteid", type="left" )
+datSS2<-join(datSS1,datSoilC,by="siteid", type="left" )
+#set up nls function to fit for each soil
+#subset so that it is only b
+datSSb<-datSS2[datSS2$param=="AmpS",]
+AmpF<-function(A0,b,depth){
+	A0*exp(-b*depth)
+}
+
+#make an amplitude list
+AmpList<-list()
+AmpDF<-list()
+for(i in 1:dim(sitesS)[1]){
+	AmpList[[i]]<-datSSb$M[datSSb$siteid==sitesS$siteid[i]]
+	AmpDF[[i]]<-data.frame(Amp=AmpList[[i]],depth=depthP[[i]])
+}
+
+N.A<-list()
+	A0est<-rep(NA,dim(sitesS)[1])
+	A0sig<-rep(NA,dim(sitesS)[1])
+	best<-rep(NA,dim(sitesS)[1])
+	bsig<-rep(NA,dim(sitesS)[1])
+
+test<-nls.control(maxiter=50000)
+for(n in 1:dim(sitesS)[1]){	
+	i=sitesS$siteid[n]
+	if(dim(AmpDF[[n]])[1]>3){
+	
+	N.A[[n]]<-nls(Amp~AmpF(A0,b,depth),control=test, data=AmpDF[[n]], start=list(A0=15,b=.007))
+	A0est[n]<-summary(N.A[[n]])$coefficients[1,1]
+	A0sig[n]<-summary(N.A[[n]])$coefficients[1,4]
+	best[n]<-summary(N.A[[n]])$coefficients[2,1]
+	bsig[n]<-summary(N.A[[n]])$coefficients[2,4]
+	}else{N.A[[n]]<-"null"}
+}
+
+
+#make a dataframe
+resultsB<-data.frame(A0=A0est,P.A0=A0sig,B=best,P.B=bsig, siteid=sitesS$siteid)
+
+plot(resultsB$siteid,resultsB$B)
+
+#now join with vegeclass
+#1	Herb barren
+#2	gramminoid tundra
+#3	tussock tundra
+#4	shrub tundra
+#5	sedge/gramminoid wetland	
+#6	needle leaf deciduous boreal
+#7	evergreen conifer borea
+#8	mixed conifer/deciduoud boreal
+
+Bresult1<-join(resultsB,datVClass, by="siteid", type="left")
+par(mfrow=c(1,2))
+plot(Bresult1$B~as.factor(Bresult1$class))
+plot(Bresult1$A0~as.factor(Bresult1$class))
+#now join with datC
+
+Bresult2<-join(resultsB,datSoilC, by="siteid", type="left")
+plot(Bresult2$soilC30,Bresult2$B,pch=19)
+
+plot(Bresult1$B~Bresult1$class)
+#see if b matches up with bd
+datBD<-read.csv("mapped_BD.csv")
+
+Bresult3<-join(resultsB,datBD, by="siteid", type="left")
+plot(Bresult3$BD,Bresult3$B,pch=19)
+
+datTC<-read.csv("soil_therm.csv")
+
+Bresult4<-join(resultsB,datTC, by="siteid", type="left")
+plot(Bresult4$TC,Bresult3$B,pch=19)
+
+datSF<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\siteinfo.csv")
+colnames(datSF)[1]<-"siteid"
+Bresult5<-join(resultsB,datSF, by="siteid", type="left")
+Bresult5$vegezfix<-ifelse(Bresult5$vege_z=="boreal","boreal",
+				ifelse(Bresult5$vege_z=="tundra", "tundra", 
+					ifelse(Bresult5$vege_z=="boreal forest", "boreal",
+						ifelse(is.na(Bresult5$vege_z),"tundra",NA))))
+					
+plot(as.factor(Bresult5$vegezfix),Bresult5$B)
+
+#plot b vs start
+
+plot(datSS$M[datSS$param=="AmpS"], datSS$M[datSS$param=="startS"])
+
+plot( datSS$M[datSS$param=="startS"],datSS$M[datSS$param=="AmpS"])
+
+plot( datSS$depth[datSS$param=="AmpS"],datSS$M[datSS$param=="AmpS"], xlim=c(0,300), pch=19)
+
+for(i in 1:dim(sitesS)[1]){
+	if(is.na(resultsB$A0[i])==FALSE){
+		points(seq(0,300, by=.1), AmpF(resultsB$A0[i],resultsB$B[i],seq(0,300, by=.1)), type="l", col="red")
+	
+	}
+
+
 }
