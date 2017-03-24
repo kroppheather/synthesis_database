5425e19a9e7acdf1565c1185cd2558382a5974f4
kroppheather <kroppheather@gmail.com>
2017-03-24 12:04:26 -0400

    started sciptfor looking at n factor patterns

--- n_model/n_model_script.r
+++ n_model/n_model_script.r
@@ -8,3 +8,18 @@ library(mcmcplots)
 setwd("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7")
 #read in n factor outputs
 datF<-read.csv("FreezeN_out.csv")
+datT<-read.csv("ThawN_out.csv")
+#siteinfo
+datI<-read.table("siteinfo.csv",sep=",", header=TRUE, na.strings=c("NaN","NA"))
+
+#species cover
+datC<-read.table("spcov.csv",sep=",", header=TRUE, na.strings=c("NaN","NA"))
+#species biomass
+datB<-read.table("spec_bio.csv",
+				sep=",", header=TRUE, na.strings=c("NaN","NA"))
+				
+#read in soil organic layer data
+datS<-read.table("soil.csv",
+				sep=",", header=TRUE, na.strings=c("NaN","NA"))
+				
+	
