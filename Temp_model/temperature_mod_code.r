23e695f7d792ef47f19a743e0fac2fac0c393d50
kroppheather <kroppheather@gmail.com>
2017-04-12 14:35:23 -0400

    fixed m2 code mistakes

--- Temp_model/temperature_mod_code.r
+++ Temp_model/temperature_mod_code.r
@@ -6,9 +6,9 @@ model{
 		Tstep1[i]<-ifelse(T.offA[i]-yearA[i]<0.25,1,0)
 		Tstep2[i]<-ifelse((T.offA[i]-yearA[i])>=0.25,ifelse((T.offA[i]-yearA[i])<0.75,1,0),0)
 		Tstep3[i]<-ifelse(T.offA[i]-yearA[i]>=0.75,1,0)
-		muA[i]<-(Tstep1[i]*(T.aveA1[SDWA[i]]-((T.aveA1[SDWA[i]]-TminA[SDWA[i]])*sin(2*3.14159265*ToffA[i]))))+
-				(Tstep2[i]*((TminA[SDWA[i]]+((TmaxA[SDWA[i]]-TminA[SDWA[i]])/2))-(((TmaxA[SDWA[i]]-TminA[SDWA[i]])/2)*sin(2*3.14159265*ToffA[i]))))+
-				(Tstep3[i]*(T.aveA2[SDWA[i]]-((TmaxA[SDWA[i]]-T.aveA2[SDWA[i]])*sin(2*3.14159265*ToffA[i]))))
+		muA[i]<-(Tstep1[i]*(T.aveA1[SDWA[i]]-((T.aveA1[SDWA[i]]-TminA[SDWA[i]])*sin(2*3.14159265*T.offA[i]))))+
+				(Tstep2[i]*((TminA[SDWA[i]]+((TmaxA[SDWA[i]]-TminA[SDWA[i]])/2))-(((TmaxA[SDWA[i]]-TminA[SDWA[i]])/2)*sin(2*3.14159265*T.offA[i]))))+
+				(Tstep3[i]*(T.aveA2[SDWA[i]]-((TmaxA[SDWA[i]]-T.aveA2[SDWA[i]])*sin(2*3.14159265*T.offA[i]))))
 		#cacluation for freezing degree day
 		#set to zero if not freezing
 		FreezeA[i]<-(1-step(TempA[i]))*TempA[i]
@@ -24,9 +24,9 @@ model{
 		TstepS1[i]<-ifelse(T.offS[i]-yearS[i]<0.25,1,0)
 		TstepS2[i]<-ifelse(T.offS[i]-yearS[i]>=0.25,ifelse(T.offS[i]-yearS[i]<0.75,1,0),0)
 		TstepS3[i]<-ifelse(T.offS[i]-yearS[i]>=0.75,1,0)
-		muS[i]<-(TstepS1[i]*(T.aveS1[SDWS[i]]-((T.aveS1[SDWS[i]]-TminS[SDWS[i]])*sin(2*3.14159265*ToffS[i]))))+
-				(TstepS2[i]*((TminS[SDWS[i]]+((TmaxS[SDWS[i]]-TminS[SDWS[i]])/2))-(((TmaxS[SDWS[i]]-TminS[SDWS[i]])/2)*sin(2*3.14159265*ToffS[i]))))+
-				(TstepS3[i]*(T.aveS2[SDWS[i]]-((TmaxS[SDWS[i]]-T.aveS2[SDWA[i]])*sin(2*3.14159265*ToffS[i]))))
+		muS[i]<-(TstepS1[i]*(T.aveS1[SDWS[i]]-((T.aveS1[SDWS[i]]-TminS[SDWS[i]])*sin(2*3.14159265*T.offS[i]))))+
+				(TstepS2[i]*((TminS[SDWS[i]]+((TmaxS[SDWS[i]]-TminS[SDWS[i]])/2))-(((TmaxS[SDWS[i]]-TminS[SDWS[i]])/2)*sin(2*3.14159265*T.offS[i]))))+
+				(TstepS3[i]*(T.aveS2[SDWS[i]]-((TmaxS[SDWS[i]]-T.aveS2[SDWS[i]])*sin(2*3.14159265*T.offS[i]))))
 		#cacluation for freezing degree day
 		#set to zero if not freezing
 		FreezeS[i]<-(1-step(TempS[i]))*TempS[i]
--- Temp_model/temperature_mod_script_linux.r
+++ Temp_model/temperature_mod_script_linux.r
@@ -568,9 +568,9 @@ for(i in 1:dim(AirIDS2)[1]){
 }
 AirrepsubV<-unlist(Airrepsub)
 
-write.table(AirrepsubV, "/home/hkropp/synthesis/output_u7/AirrepID.csv", sep=",", row.names=FALSE)
+write.table(AirrepsubV, "/home/hkropp/synthesis/output_u7m2/AirrepID.csv", sep=",", row.names=FALSE)
 print("repA_out")
-write.table(SoilrepsubV, "/home/hkropp/synthesis/output_u7/SoilrepID.csv", sep=",", row.names=FALSE)
+write.table(SoilrepsubV, "/home/hkropp/synthesis/output_u7m2/SoilrepID.csv", sep=",", row.names=FALSE)
 print("reps_out")
 #model
 #write.table(AirM2,"c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u6\\Tmod4rep\\rep\\Tair_model.csv",sep=",",row.names=FALSE)
@@ -609,20 +609,20 @@ print("samples done done")
 Mod.out<-summary(codaobj.init)
 
 
-write.table(Mod.out$statistics, "/home/hkropp/synthesis/output_u7/Temp_mod7_stats.csv",
+write.table(Mod.out$statistics, "/home/hkropp/synthesis/output_u7m2/Temp_mod7_stats.csv",
 			sep=",",row.names=TRUE)
-write.table(Mod.out$quantiles, "/home/hkropp/synthesis/output_u7/Temp_mod7_quant.csv",
+write.table(Mod.out$quantiles, "/home/hkropp/synthesis/output_u7m2/Temp_mod7_quant.csv",
 			sep=",",row.names=TRUE)
 			
 print("summary out")	
 
 
-save(codaobj.init, file="/home/hkropp/synthesis/output_u7/mod7_coda.R")
+save(codaobj.init, file="/home/hkropp/synthesis/output_u7m2/mod7_coda.R")
 			
 print("coda out")	
 
 			
-mcmcplot(codaobj.init, dir="/home/hkropp/synthesis/output_u7")		
+mcmcplot(codaobj.init, dir="/home/hkropp/synthesis/output_u7m2")		
 #get summary and save to file
 
 print("mcmcplot out")	
@@ -630,12 +630,12 @@ print("mcmcplot out")
 
 #need to write ids to table
 
-write.table(AirIDS2,"/home/hkropp/synthesis/output_u7/AirIDS.csv", sep=",", row.names=FALSE)
-write.table(SoilIDS2,"/home/hkropp/synthesis/output_u7/SoilIDS.csv", sep=",", row.names=FALSE)
+write.table(AirIDS2,"/home/hkropp/synthesis/output_u7m2/AirIDS.csv", sep=",", row.names=FALSE)
+write.table(SoilIDS2,"/home/hkropp/synthesis/output_u7m2/SoilIDS.csv", sep=",", row.names=FALSE)
 
-write.table(site.heightA,"/home/hkropp/synthesis/output_u7/AirIDS_SD.csv", sep=",", row.names=FALSE)
-write.table(site.depthidS,"/home/hkropp/synthesis/output_u7/SoilIDS_SD.csv", sep=",", row.names=FALSE)
+write.table(site.heightA,"/home/hkropp/synthesis/output_u7m2/AirIDS_SD.csv", sep=",", row.names=FALSE)
+write.table(site.depthidS,"/home/hkropp/synthesis/output_u7m2/SoilIDS_SD.csv", sep=",", row.names=FALSE)
 
-write.table(IDforCombo, "/home/hkropp/synthesis/output_u7/NfactorIDS.csv",sep=",", row.names=FALSE)
+write.table(IDforCombo, "/home/hkropp/synthesis/output_u7m2/NfactorIDS.csv",sep=",", row.names=FALSE)
 print("ID write out")	
 			
