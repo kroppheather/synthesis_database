d4536672cc644dd688b9fccaf2dc2be366996ffa
kroppheather <kroppheather@gmail.com>
2017-04-24 14:02:18 -0400

    plotted mixing model results

--- Temp_model/temperature_mod_plot.r
+++ Temp_model/temperature_mod_plot.r
@@ -1,17 +1,19 @@
 library(plyr)
 #read in data
-setwd("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod2\\output_u7m2")
+setwd("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod6\\output_u7m6")
 
 datAI<-read.csv("AirIDS.csv")
 datSI<-read.csv("SoilIDS.csv")
-datM<-read.csv("Temp_mod7_stats.csv")
-datQ<-read.csv("Temp_mod7_quant.csv")
+datM<-read.csv("Temp_mod6_stats.csv")
+datQ<-read.csv("Temp_mod6_quant.csv")
 
 datAIS<-read.csv("AirIDS_SD.csv")
 datSIS<-read.csv("SoilIDS_SD.csv")
 
 datAM<-read.csv("Tair_model.csv")
 datSM<-read.csv("Tsoil_model.csv")
+
+datSF<-read.csv("startF.csv")
 
 datSM$decdateA<-datSM$decdate-1991
 datAM$decdateA<-datAM$decdate-1991
@@ -25,10 +27,24 @@ dexps<-"\\[*[[:digit:]]*\\]"
 datC$parms1<-gsub(dexps,"",rownames(datC))
 
 #subset first to only look at soil parms
-datCS<-datC[datC$parms1=="T.aveS1"|datC$parms1=="T.aveS2"|datC$parms1=="TmaxS"|datC$parms1=="TminS"|datC$parms1=="startS",]
+datCS<-datC[datC$parms1=="T.aveS1"|datC$parms1=="T.aveS2"|datC$parms1=="TmaxS"|datC$parms1=="TminS"|datC$parms1=="startS"|datC$parms1=="aZero"|datC$parms1=="bZero",]
+
+#pull out zeroC value
+zeroMeanP<-datC[datC$parms1=="zeroC",]
+zeroMean<-zeroMeanP$Mean
+zeroSD<-zeroMeanP$SD
+
+
+datRS<-datC[datC$parms1=="TempS.rep",]
+
 
 #now add id number
 dexps2<-"\\D"
+
+datRS$ID<-gsub(dexps2,"",rownames(datRS))
+
+datRR<-data.frame(M=datRS$Mean, pc2.5=datRS$X2.5.,pc97.5=datRS$X97.5., param=datRS$parms1,ID=datRS$ID)
+
 #pull out names
 pnames<-rownames(datCS)
 #need to split because there are numbers in param names
@@ -57,12 +73,15 @@ sTa2<-datC[datC$param=="T.aveS2",]
 sTmin<-datC[datC$param=="TminS",]
 sTmax<-datC[datC$param=="TmaxS",]
 sTstart<-datC[datC$param=="startS",]
+sAzero<-datC[datC$param=="aZero",]
+sBzero<-datC[datC$param=="bZero",]
 colnames(sTa1)[5]<-"SDWS"
 colnames(sTa2)[5]<-"SDWS"
 colnames(sTmin)[5]<-"SDWS"
 colnames(sTmax)[5]<-"SDWS"
 colnames(sTstart)[5]<-"SDS"
-
+colnames(sAzero)[5]<-"SDWS"
+colnames(sBzero)[5]<-"SDWS"
 
 #now join with IDS
 sTa1<-join(sTa1, datSI, by="SDWS", type="left")
@@ -70,9 +89,19 @@ sTa2<-join(sTa2, datSI, by="SDWS", type="left")
 sTmin<-join(sTmin, datSI, by="SDWS", type="left")
 sTmax<-join(sTmax, datSI, by="SDWS", type="left")
 sTstart<-join(sTstart, datSIS, by="SDS", type="left")
+sAzero<-join(sAzero, datSI, by="SDWS", type="left")
+sBzero<-join(sBzero, datSI, by="SDWS", type="left")
 
 #now make plots by site to see how this compares
 #set up sine function
+zeroF<-function(a,b,tdiff){
+	a*exp(-b*tdiff)
+
+
+}
+
+
+
 Tsine1<-function(Tave1,Tmin,Tyear,Tstart){
 		
 		ifelse(((Tyear-Tstart)-floor(Tyear))<.25,
@@ -100,6 +129,11 @@ for(i in 1:dim(sitesS)[1]){
 	wyearP[[i]]<-unique(datSI$wyear[datSI$siteid==sitesS$siteid[i]])
 }
 
+#pull out data for temperature diff
+datDID<-ifelse(datSF$x==-1,NA,datSM$indexI-1)
+datSM$datTdev<-ifelse(is.na(datDID),-999,abs(datSM$T[datDID]))
+
+
 #set up list of x variables for sine function
 xP<-list()
 xPwyear<-list()
@@ -117,6 +151,26 @@ DFtest7<-list()
 Ysine1<-list()
 Ysine2<-list()
 Ysine3<-list()
+Ztest1<-list()
+Ztest2<-list()
+ZP1<-list()
+DDx<-list()
+DDx1<-list()
+DDx2<-list()
+DDx3<-list()
+DDx4<-list()
+DDx5<-list()
+DDx6<-list()
+DDsine1<-list()
+DDsine2<-list()
+DDsine3<-list()
+DDsineC<-list()
+DDmix<-list()
+DDfilter<-list()
+DDfilter2<-list()
+DDprob<-list()
+
+
 for(i in 1:dim(sitesS)[1]){
 	xP[[i]]<-seq(floor(min(datSM$decdateA[datSM$siteid==sitesS$siteid[i]])),
 				ceiling(max(datSM$decdateA[datSM$siteid==sitesS$siteid[i]])),
@@ -162,8 +216,66 @@ for(i in 1:dim(sitesS)[1]){
 	Ysine2[[i]]<-Tsine2(DFtest7[[i]]$MMI,DFtest7[[i]]$MMA,DFtest7[[i]]$x,DFtest7[[i]]$MS)
 	Ysine3[[i]]<-Tsine3(DFtest7[[i]]$MT2,DFtest7[[i]]$MMA,DFtest7[[i]]$x,DFtest7[[i]]$MS)
 	
+	#now pull out the temperature difference for each site
 	
+	
+	#now look at zero mixing function
+	Ztest1[[i]]<-data.frame(AZ=sAzero$M[sAzero$siteid==sitesS$siteid[i]],
+		depth=sAzero$depth[sAzero$siteid==sitesS$siteid[i]],
+				wyear=as.numeric(sAzero$wyear[sAzero$siteid==sitesS$siteid[i]]))
+	Ztest2[[i]]<-data.frame(BZ=sBzero$M[sBzero$siteid==sitesS$siteid[i]],
+		depth=sBzero$depth[sBzero$siteid==sitesS$siteid[i]],
+				wyear=as.numeric(sBzero$wyear[sBzero$siteid==sitesS$siteid[i]]))
+	
+	ZP1[[i]]<-join(Ztest1[[i]],Ztest2[[i]], by=c("wyear", "depth"), type="left")
+	
+	DDx[[i]]<-datSM[datSM$siteid==sitesS$siteid[i],]
+	
+	DDx1[[i]]<-join(DFP1[[i]],DDx[[i]], by=c("wyear", "depth"), type="right")
+	DDx2[[i]]<-join(DFP2[[i]],DDx1[[i]], by=c("wyear", "depth"), type="right")
+	DDx3[[i]]<-join(DFP3[[i]],DDx2[[i]], by=c( "depth"), type="right")
+	DDx4[[i]]<-join(DFP4[[i]],DDx3[[i]], by=c("wyear", "depth"), type="right")
+	DDx5[[i]]<-join(DFP5[[i]],DDx4[[i]], by=c("wyear", "depth"), type="right")
+	DDx6[[i]]<-join(ZP1[[i]],DDx5[[i]], by=c("wyear", "depth"), type="right")
+	
+	#DDsine1[[i]]<-ifelse(is.na(Tsine1(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS)),0,Tsine1(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS))
+	#DDsine2[[i]]<-ifelse(is.na(Tsine2(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS)),0,Tsine2(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS))
+	#DDsine3[[i]]<-ifelse(is.na(Tsine3(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS)),0,Tsine3(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS))
+	#DDsineC[[i]]<-DDsine1[[i]]+DDsine2[[i]]+DDsine3[[i]]
+	
+
+	DDmix[[i]]<-zeroF(DDx6[[i]]$AZ,DDx6[[i]]$BZ,DDx6[[i]]$datTdev)
+	DDfilter[[i]]<-ifelse(DDmix[[i]]==Inf|DDx6[[i]]$datTdev==-999,0, DDmix[[i]])
+	DDfilter2[[i]]<-ifelse(is.na(DDfilter[[i]]),0,DDfilter[[i]])
+
+		DDsine1[[i]]<-Tsine1(DDx6[[i]]$MT,DDx6[[i]]$MMI,DDx6[[i]]$decdateA,DDx6[[i]]$MS)
+		DDsine2[[i]]<-Tsine2(DDx6[[i]]$MMI,DDx6[[i]]$MMA,DDx6[[i]]$decdateA,DDx6[[i]]$MS)
+		DDsine3[[i]]<-Tsine3(DDx6[[i]]$MT2,DDx6[[i]]$MMA,DDx6[[i]]$decdateA,DDx6[[i]]$MS)
 }
+
+DDFilterDF<-ldply(DDfilter2,data.frame)
+colnames(DDFilterDF)<-"Prob"
+
+Xest<-numeric()
+for(i in 1:dim(DDFilterDF)[1]){
+	Xest[i]<-rbinom(1,1,DDFilterDF$Prob[i])
+
+}
+		
+#now combine back together
+Xlist<-list()
+ZeroMix1<-list()
+ZeroMix2<-list()
+ZeroMix3<-list()
+for(i in 1:dim(sitesS)[1]){
+	Xlist[[i]]<-Xest[datSM$siteid==sitesS$siteid[i]]
+	ZeroMix1[[i]]<-(Xlist[[i]]*zeroMean)+((1-Xlist[[i]])*DDsine1[[i]])
+	ZeroMix2[[i]]<-(Xlist[[i]]*zeroMean)+((1-Xlist[[i]])*DDsine2[[i]])
+	ZeroMix3[[i]]<-(Xlist[[i]]*zeroMean)+((1-Xlist[[i]])*DDsine3[[i]])
+}
+				
+
+
 		
 
 
@@ -181,7 +293,7 @@ test[[n]]<-datSS$M[datSS$siteid==i&datSS$depth==depthP[[n]][j]&datSS$wyear==xPwy
 
 for(n in 1:dim(sitesS)[1]){	
 	i<-sitesS$siteid[n]
-	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod2\\plots\\site",i,".jpg"),
+	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod6\\plots\\site",i,".jpg"),
 			width=1500,height=1000, units="px")
 	par(mai=c(2,2,2,2))
 	plot(c(0,1),c(0,1),type="n",xlim=c(min(datSM$decdateA[datSM$siteid==i]),max(datSM$decdateA[datSM$siteid==i])),
@@ -209,232 +321,112 @@ for(n in 1:dim(sitesS)[1]){
 	dev.off()
 }
 
-######################################################################################################################
-##########below is old code. Need to fix for new index
+################now make plots to see what mixing function looks like
+Tdiff<-seq(0,45, by=.1)
 
 
-#now look at how the amplitude compares for sites with the more than three depths
-
-#depth count
-depthn<-aggregate(datSS$M[datSS$param=="AmpS"],by=list(datSS$siteid[datSS$param=="AmpS"]), FUN="length")
-dim(depthn[depthn$x>2,])
-dim(depthn[depthn$x>1,])
-
-#subset data to plot by 
-depthpn<-depthn[depthn$x>1,]
-
-#now subset datSS Amplitude by sites to use
-
-ssA<-datSS[datSS$param=="AmpS",]
-colnames(depthpn)<-c("siteid", "number")
-ssA<-join(depthpn,ssA, by="siteid", type="inner")
-#get unique site list
-site.ssA<-data.frame(siteid=unique(ssA$siteid))
-site.ssA$ind<-seq(1,dim(site.ssA)[1])
-
-#make plots comparing amplitude across depth for all sites
-
-for(n in 1:dim(site.ssA)[1]){
-	i<-site.ssA$siteid[n]
-	#save plot to file
-	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod1\\output_u7\\plots\\site",i,".jpg"),
-			width=1500,height=1000, units="px")
-	#make blank plot
-	plot(c(0,1),c(0,1), type="n", xlim=c(min(ssA$pc2.5[ssA$siteid==i])-1,max(ssA$pc97.5[ssA$siteid==i])+1),
-			ylim=c(max(ssA$depth[ssA$siteid==i])+5,min(ssA$depth[ssA$siteid==i]-5)), xaxs="i",yaxs="i",
-			xlab="Amplitude",ylab="Depth")
-	#add amplitude and error
-	points(ssA$M[ssA$siteid==i], ssA$depth[ssA$siteid==i], pch=19, cex=2, col="deepskyblue3")
-	arrows(ssA$pc2.5[ssA$siteid==i],ssA$depth[ssA$siteid==i],ssA$pc97.5[ssA$siteid==i],ssA$depth[ssA$siteid==i],code=0)
-	#add site lable
-	text(min(ssA$pc2.5[ssA$siteid==i])-.1,min(ssA$depth[ssA$siteid==i])-4,paste("siteid=", i), cex=2)
-	dev.off()
-}
-ssS<-datSS[datSS$param=="startS",]
-colnames(depthpn)<-c("siteid", "depthn")
-ssS<-join(depthpn,ssS, by="siteid", type="inner")
-#get unique site list
-site.ssS<-data.frame(siteid=unique(ssS$siteid))
-site.ssS$ind<-seq(1,dim(site.ssS)[1])
-
-#make plots comparing startS across depth for all sites
-	fit<-lm(ssS$M[ssS$siteid==1]~ssS$depth[ssS$siteid==1])
-	abline(fit)
-	fit$coefficients[1]
-	summary(fit)$r.squared
-for(n in 1:dim(site.ssS)[1]){
-	i<-site.ssS$siteid[n]
-	#save plot to file
-	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u6\\T_mod2_out\\startdep\\site",i,".jpg"),
-			width=1500,height=1000, units="px")
-	#make blank plot
-	plot(c(0,1),c(0,1), type="n", ylim=c(min(ssS$pc2.5[ssS$siteid==i])-.2,max(ssS$pc97.5[ssS$siteid==i])+.2),
-			xlim=c(min(ssS$depth[ssS$siteid==i]-1),max(ssS$depth[ssS$siteid==i])+1), xaxs="i",yaxs="i",
-			xlab="Depth",ylab="Start")
-	#add amplitude and error
-	points( ssS$depth[ssS$siteid==i],ssS$M[ssS$siteid==i], pch=19, cex=2, col="deepskyblue3")
-	arrows(ssS$depth[ssS$siteid==i],ssS$pc2.5[ssS$siteid==i],ssS$depth[ssS$siteid==i],ssS$pc97.5[ssS$siteid==i],code=0)
-	#add site lable
-	text(min(ssS$depth[ssS$siteid==i])+4,max(ssS$pc97.5[ssS$siteid==i]),paste("siteid=", i), cex=2)
-	fit<-lm(ssS$M[ssS$siteid==i]~ssS$depth[ssS$siteid==i])
-	abline(fit)
-	text(min(ssS$depth[ssS$siteid==i])+10,max(ssS$pc97.5[ssS$siteid==i])+.1, paste("rsquared =", round(summary(fit)$r.squared,2)), cex=1.5)
-	text(min(ssS$depth[ssS$siteid==i])+15,max(ssS$pc97.5[ssS$siteid==i])+.17, paste("start =", round(fit$coefficients[1],5),"+", round(fit$coefficients[2],5),"*depth"), cex=1.5)
-	
-	dev.off()
-}
-
-
-
-#only look at air vs soil
-datSA<-datS[datS$param=="AmpA"|datS$param=="T.aveA"|datS$param=="startA",]
-datSS<-datS[datS$param=="AmpS"|datS$param=="T.aveS"|datS$param=="startS",]	
-#plot the soil
 for(n in 1:dim(sitesS)[1]){	
 	i<-sitesS$siteid[n]
-	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u6\\T_mod2_out\\site",i,".jpg"),
+	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod6\\plots\\tdev\\site",i,".jpg"),
 			width=1500,height=1000, units="px")
+	par(mai=c(2,2,2,2))
+	plot(c(0,1),c(0,1),type="n",xlim=c(0,45),
+								ylim=c(0,1),
+								xlab="Temperature Deviance from zero", ylab="Probability", cex.axis=2, cex.lab=3)
+								
+	for(j in 1:length(ZP1[[n]]$AZ)){
+			points(Tdiff,zeroF(ZP1[[n]]$AZ[j],
+								ZP1[[n]]$BZ[j],Tdiff),
+				col=colP[j],type="l",lwd=2,lty=1)
+			
+		}
+	
 
+	legend(25,1, paste("depth=", ZP1[[n]]$depth, "year=",ZP1[[n]]$wyear),
+			col=colP[1:length(ZP1[[n]]$AZ)],pch=19, bty="n", cex=2)
+			
+	text(20,.95, paste("siteid=", sitesS$siteid[n]), cex=3)
+	dev.off()
+}
+
+
+#now check the temp predictions
+
+#read in ID
+datSRC<-read.csv("SoilrepID.csv")
+#pull out t values
+datTR<-datSM$T[datSRC$x]
+
+
+plot(datTR, datRR$M, pch=19, ylim=c(-40,25), xlim=c(-40,25))
+fitS<-lm( datRR$M~datTR)
+summary(fitS)
+abline(fitS, lty=3, lwd=2)
+abline(0,1,col="red", lwd=2)
+
+
+
+
+#########################################################################
+##########try plotting both
+
+for(n in 1:dim(sitesS)[1]){	
+	i<-sitesS$siteid[n]
+	jpeg(file=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\Tmod6\\plots\\mixing\\site",i,".jpg"),
+			width=1500,height=1000, units="px")
+	par(mai=c(2,2,2,2))
 	plot(c(0,1),c(0,1),type="n",xlim=c(min(datSM$decdateA[datSM$siteid==i]),max(datSM$decdateA[datSM$siteid==i])),
-								ylim=c(min(datSM$T[datSM$siteid==i]),max(datSM$T[datSM$siteid==i])),
-								xlab="Time", ylab="Temperature")
+								ylim=c(min(na.omit(datSM$T[datSM$siteid==i])),max(na.omit(datSM$T[datSM$siteid==i]))),
+								xlab="Water Year since 1991", ylab="Temperature", cex.axis=2, cex.lab=3)
 		
 		
 	for(j in 1:length(depthP[[n]])){
 		points(datSM$decdateA[datSM$siteid==i&datSM$depth==depthP[[n]][j]],
 				datSM$T[datSM$siteid==i&datSM$depth==depthP[[n]][j]],
 				pch=19, col=colP[j])
-		points(xP[[n]],Tsine(datSS$M[datSS$siteid==i&datSS$depth==depthP[[n]][j]&datSS$param=="T.aveS"],
-						datSS$M[datSS$siteid==i&datSS$depth==depthP[[n]][j]&datSS$param=="AmpS"],xP[[n]],
-						datSS$M[datSS$siteid==i&datSS$depth==depthP[[n]][j]&datSS$param=="startS"]),
+		points(DDx6[[n]]$decdateA[DDx6[[n]]$depth==depthP[[n]][j]],ZeroMix1[[n]][DDx6[[n]]$depth==depthP[[n]][j]],
 				col=colP[j],type="l",lwd=2,lty=1)
+		points(DDx6[[n]]$decdateA[DDx6[[n]]$depth==depthP[[n]][j]],ZeroMix2[[n]][DDx6[[n]]$depth==depthP[[n]][j]],
+				col=colP[j],type="l",lwd=2,lty=1)
+			points(DDx6[[n]]$decdateA[DDx6[[n]]$depth==depthP[[n]][j]],ZeroMix3[[n]][DDx6[[n]]$depth==depthP[[n]][j]],
+				col=colP[j],type="l",lwd=2,lty=1)		
+
+
 	}
 
-	legend(min(datSM$decdateA[datSM$siteid==i])+.001,max(datSM$T[datSM$siteid==i])-.25, paste("depth=", depthP[[n]]),
-			col=colP[1:length(depthP[[n]])],pch=19, bty="n", cex=1.5)
+	legend(min(na.omit(datSM$decdateA[datSM$siteid==i]))+.001,max(na.omit(datSM$T[datSM$siteid==i]))-.25, paste("depth=", depthP[[n]]),
+			col=colP[1:length(depthP[[n]])],pch=19, bty="n", cex=3)
 			
-	text(min(datSM$decdateA[datSM$siteid==i])+.5,max(datSM$T[datSM$siteid==i])-.5, paste("siteid=", sitesS$siteid[i]), cex=2)
+	text(min(datSM$decdateA[datSM$siteid==i])+2,max(datSM$T[datSM$siteid==i])-2, paste("siteid=", sitesS$siteid[n]), cex=3)
 	dev.off()
 }
 
 
-#read in some of the site and vegetation data
 
-#site vegetation class
-datVClass<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\vege_class.csv")
-#site mapped soil C
-datSoilC<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\soilc_30cm.csv")
-
-
-#match up soil and vege measurements
-colnames(datVClass)<-c("siteid","class")
-colnames(datSoilC)<-c("siteid", "soilC30")
-datSS1<-join(datSS,datVClass,by="siteid", type="left" )
-datSS2<-join(datSS1,datSoilC,by="siteid", type="left" )
-#set up nls function to fit for each soil
-#subset so that it is only b
-datSSb<-datSS2[datSS2$param=="AmpS",]
-AmpF<-function(A0,b,depth){
-	A0*exp(-b*depth)
-}
-
-#make an amplitude list
-AmpList<-list()
-AmpDF<-list()
-for(i in 1:dim(sitesS)[1]){
-	AmpList[[i]]<-datSSb$M[datSSb$siteid==sitesS$siteid[i]]
-	AmpDF[[i]]<-data.frame(Amp=AmpList[[i]],depth=depthP[[i]])
-}
-
-N.A<-list()
-	A0est<-rep(NA,dim(sitesS)[1])
-	A0sig<-rep(NA,dim(sitesS)[1])
-	best<-rep(NA,dim(sitesS)[1])
-	bsig<-rep(NA,dim(sitesS)[1])
-
-test<-nls.control(maxiter=50000)
-for(n in 1:dim(sitesS)[1]){	
-	i=sitesS$siteid[n]
-	if(dim(AmpDF[[n]])[1]>3){
-	
-	N.A[[n]]<-nls(Amp~AmpF(A0,b,depth),control=test, data=AmpDF[[n]], start=list(A0=15,b=.007))
-	A0est[n]<-summary(N.A[[n]])$coefficients[1,1]
-	A0sig[n]<-summary(N.A[[n]])$coefficients[1,4]
-	best[n]<-summary(N.A[[n]])$coefficients[2,1]
-	bsig[n]<-summary(N.A[[n]])$coefficients[2,4]
-	}else{N.A[[n]]<-"null"}
-}
-
-
-#make a dataframe
-resultsB<-data.frame(A0=A0est,P.A0=A0sig,B=best,P.B=bsig, siteid=sitesS$siteid)
-
-plot(resultsB$siteid,resultsB$B)
-
-#join start with B
-
-fitS<-list()	
-for(n in 1:dim(sitesS)[1]){
-	i=sitesS$siteid[n]
-
-	fitS[[i]]<-lm(ssS$M[ssS$siteid==i]~ssS$depth[ssS$siteid==i])
-
-}
-
-#now join with vegeclass
-#1	Herb barren
-#2	gramminoid tundra
-#3	tussock tundra
-#4	shrub tundra
-#5	sedge/gramminoid wetland	
-#6	needle leaf deciduous boreal
-#7	evergreen conifer borea
-#8	mixed conifer/deciduoud boreal
-
-Bresult1<-join(resultsB,datVClass, by="siteid", type="left")
-par(mfrow=c(1,2))
-plot(Bresult1$B~as.factor(Bresult1$class))
-plot(Bresult1$A0~as.factor(Bresult1$class))
-#now join with datC
-
-Bresult2<-join(resultsB,datSoilC, by="siteid", type="left")
-plot(Bresult2$soilC30,Bresult2$B,pch=19)
-
-plot(Bresult1$B~Bresult1$class)
-#see if b matches up with bd
-datBD<-read.csv("mapped_BD.csv")
-
-Bresult3<-join(resultsB,datBD, by="siteid", type="left")
-plot(Bresult3$BD,Bresult3$B,pch=19)
-
-datTC<-read.csv("soil_therm.csv")
-
-Bresult4<-join(resultsB,datTC, by="siteid", type="left")
-plot(Bresult4$TC,Bresult3$B,pch=19)
-
-datSF<-read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_3\\siteinfo.csv")
-colnames(datSF)[1]<-"siteid"
-Bresult5<-join(resultsB,datSF, by="siteid", type="left")
-Bresult5$vegezfix<-ifelse(Bresult5$vege_z=="boreal","boreal",
-				ifelse(Bresult5$vege_z=="tundra", "tundra", 
-					ifelse(Bresult5$vege_z=="boreal forest", "boreal",
-						ifelse(is.na(Bresult5$vege_z),"tundra",NA))))
-					
-plot(as.factor(Bresult5$vegezfix),Bresult5$B)
-
-#plot b vs start
-
-plot(datSS$M[datSS$param=="AmpS"], datSS$M[datSS$param=="startS"])
-
-plot( datSS$M[datSS$param=="startS"],datSS$M[datSS$param=="AmpS"])
-
-plot( datSS$depth[datSS$param=="AmpS"],datSS$M[datSS$param=="AmpS"], xlim=c(0,300), pch=19)
-
-for(i in 1:dim(sitesS)[1]){
-	if(is.na(resultsB$A0[i])==FALSE){
-		points(seq(0,300, by=.1), AmpF(resultsB$A0[i],resultsB$B[i],seq(0,300, by=.1)), type="l", col="red")
-	
+	par(mai=c(2,2,2,2))
+	plot(c(0,1),c(0,1),type="n",xlim=c(min(na.omit(datSM$decdateA[datSM$siteid==2])),max(na.omit(datSM$decdateA[datSM$siteid==2]))),
+								ylim=c(min(na.omit(datSM$T[datSM$siteid==2])),max(na.omit(datSM$T[datSM$siteid==2]))),
+								xlab="Water Year since 1991", ylab="Temperature", cex.axis=2, cex.lab=3)
+		
+		
+	for(j in 1:length(depthP[[2]])){
+		points(datSM$decdateA[datSM$siteid==2&datSM$depth==depthP[[1]][j]],
+				datSM$T[datSM$siteid==2&datSM$depth==depthP[[1]][j]],
+				pch=19, col=colP[j])
+		}
+	for(j in 1:length(depthP[[2]])){		
+				
+			points(DDx6[[1]]$decdateA[DDx6[[1]]$depth==depthP[[1]][j]],ZeroMix1[[1]][DDx6[[1]]$depth==depthP[[1]][j]],
+				col=colP[j],type="l",lwd=2,lty=1)
+		points(DDx6[[1]]$decdateA[DDx6[[1]]$depth==depthP[[1]][j]],ZeroMix2[[1]][DDx6[[1]]$depth==depthP[[1]][j]],
+				col=colP[j],type="l",lwd=2,lty=1)
+			points(DDx6[[1]]$decdateA[DDx6[[1]]$depth==depthP[[1]][j]],ZeroMix3[[1]][DDx6[[1]]$depth==depthP[[1]][j]],
+				col=colP[j],type="l",lwd=2,lty=1)
 	}
 
+	legend(min(na.omit(datSM$decdateA[datSM$siteid==i]))+.001,max(na.omit(datSM$T[datSM$siteid==i]))-.25, paste("depth=", depthP[[n]]),
+			col=colP[1:length(depthP[[n]])],pch=19, bty="n", cex=3)
+			
+	text(min(datSM$decdateA[datSM$siteid==i])+2,max(datSM$T[datSM$siteid==i])-2, paste("siteid=", sitesS$siteid[n]), cex=3)
 
 }
