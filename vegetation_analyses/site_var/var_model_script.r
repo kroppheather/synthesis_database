59957fce309ccc671aaa270de2264a728b2a4a3c
kroppheather <kroppheather@gmail.com>
2017-05-24 10:15:02 -0400

    model set up for vegetation community

--- vegetation_analyses/site_var/var_model_script.r
+++ vegetation_analyses/site_var/var_model_script.r
@@ -4,7 +4,10 @@
 ######## to look at variability in decoupling     #####
 #######################################################
 library(plyr)
-
+library(rjags)
+library(coda)
+library(xtable)
+library(mcmcplots)
 setwd("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\mod10_out")
 #read in data
 datN<-read.csv("nfactor.csv")
@@ -69,3 +72,68 @@ datTmin<-join(datTmin, datTminA, by=c("siteid","wyear"), type="left")
 
 datPS<-join(datPS,datPSA,by=c("siteid","wyear"), type="left")
 datPW<-join(datPW,datPWA,by=c("siteid","wyear"), type="left")
+
+datDZ<-join(datDZ,datTminA, by=c("siteid","wyear"), type="left")
+
+datAll<-list(datNF,datNT,datTmax,datTmin,datPS,datPW,datDZ)
+#get the average Air value for covariate centering
+roundI<-c(0,0,0,0,2,2,0)
+TexM<-numeric(0)
+for(i in 1:7){
+	TexM[i]<-round(mean(datAll[[i]]$MeanA),roundI[i])
+
+}
+data.name<-c("nfreeze","nthaw","Tmax","Tmin","Peakmax","Peakmin","DayZero")
+####################################################################
+####### get model runs ready. Run a model for each dataset  ########
+####################################################################
+samplelist<-c("b1","b2","b3","sigM","sigV","rep.Xobs")
+
+for(i in 1:7){
+	datalist<-list(Nobs=dim(datAll[[i]])[1], Xobs=datAll[[i]]$Mean,
+					vegeC=datAll[[i]]$class, depth=datAll[[i]]$depth,
+					airM=datAll[[i]]$MeanA,airM.bar=TexM[i],
+					meas.sig=datAll[[i]]$SD, Nvege=8)
+	
+	
+	X.modI<-jags.model(file="c:\\Users\\hkropp\\Documents\\GitHub\\synthesis_database\\vegetation_analyses\\site_var\\var_model_code.r",
+						data=datalist,
+						n.adapt=5000,
+						n.chains=3)				
+					
+	print(paste("initialize data ",i ))		
+	#specify sample run				
+	n.iter.i=500000
+	n.thin=25
+	codaobj.init = coda.samples(X.modI,variable.names=samplelist,
+                       n.iter=n.iter.i, thin=n.thin)
+					   
+					   
+	print(paste("samples done data= ", i))
+
+	#pull out model stats
+	Mod.out<-summary(codaobj.init)
+
+	write.table(Mod.out$statistics, 
+			paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\mod10_out\\model\\var\\mod1\\",data.name[i],"Temp_mod_stats.csv"),
+			sep=",",row.names=TRUE)
+	write.table(Mod.out$quantiles, paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\mod10_out\\model\\var\\mod1\\",data.name[i],"Temp_mod_quant.csv"),
+			sep=",",row.names=TRUE)
+			
+print(paste("summary out data ",i)	)
+
+
+#run mcmc plots on key params
+dir.create(paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\mod10_out\\model\\var\\mod1\\",data.name[i]))	
+
+mcmcplot(codaobj.init, parms=c("b1","b2","b3","sigV"),
+			dir=paste0("c:\\Users\\hkropp\\Google Drive\\raw_data\\analysis_u7\\mod10_out\\model\\var\\mod1\\",data.name[i]))		
+#get summary and save to file
+
+print(paste("mcmcplot out data ", i))	
+
+
+}
+
+
+
