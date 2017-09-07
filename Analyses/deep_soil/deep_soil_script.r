152ea404d817bfa7468ddaf01e325288b49b86d4
kroppheather <kroppheather@gmail.com>
2017-09-07 15:47:43 -0400

    finished plots for deep soil temperature

--- Analyses/deep_soil/deep_soil_script.r
+++ Analyses/deep_soil/deep_soil_script.r
@@ -18,7 +18,7 @@
 ##########################################################
 
 #libraries loaded in source script: plyr
-
+library(wesanderson)
 
 ##########################################
 ##first grab soil temperature parameters##
@@ -107,19 +107,123 @@ datAll<-list(datNF,datNT,datTmax,datTmin,datPS,datPW,datDZ)
 
 IDsTable <- unique(data.frame(siteid=datNF$siteid, wyear=datNF$wyear, depth=datNF$depth,
 						Vclass=datNF$Vclass, biome=datNF$biome))
+						
+IDsTable$newB <- ifelse(IDsTable$Vclass<=3 |IDsTable$Vclass==6, "gtundra",
+					ifelse(IDsTable$Vclass==4 |IDsTable$Vclass==5, "stundra","boreal"))						
 
+#create a color table
 
+colVCT <- unique(data.frame(Vclass=IDsTable$Vclass))
+colVCT$CName <- c("mixed boreal", "evergreen boreal", "tall shrub", "gramminoid", "short shrub", "tussock", "wetland", "herb-bare")
 
+colVCT$coli <- c("tomato3","lightsteelblue3","tomato3","lightsteelblue3","lightsteelblue3","tomato3",
+					"lightgoldenrod4", "grey30")
+IDsTable <- join(IDsTable, colVCT, by="Vclass", type="left")
+#get unique site year to plot in each biome
+
+BorealID <- IDsTable[IDsTable$newB=="boreal",]						
+	BorealSites<- unique(data.frame(siteid=BorealID$siteid, wyear=BorealID$wyear))					
+gTundraID <- IDsTable[IDsTable$newB=="gtundra",]						
+gTundraSites<- unique(data.frame(siteid=gTundraID$siteid, wyear=gTundraID$wyear))	
+sTundraID <- IDsTable[IDsTable$newB=="stundra",]						
+sTundraSites<- unique(data.frame(siteid=sTundraID$siteid, wyear=sTundraID$wyear))	
+
+	
 ##############
 #start by looking at n factors across depth with each biome seperated
-wb <- 20
-hb <- 25
+wb <- 35
+hb <- 50
 
 #get the maximum depth to plot
-yh <- max()
+yu <- 0
+yh <- 310
+#datNF,datNT,datTmax,datTmin,datPS,datPW,datDZ
+xl <- c(0,0,-1,-40,.5,0,0)
+xh <- c(1.7,1.7,20,0,1,.6,255)
+yla<- -0
+yha <- 300
+yi <- -50
+xla <- c(0,0,0,-35,.5,0,0)
+xha <-  c(1.5,1.5,18,-5,.9,.5,200)
+xi <- c(.3,.3,3,10,.1,.1,50)
+xl1 <-c(.8,.8,10,-15,.7,.3,200)
+yl1 <-c(250,250,250,250,250,250,250)
+dataType <- c("Freezing n-factor", "Thawing n-factor", "Temperature maximum (C)", "Temperature minimum (C)",
+				"Time of temperature maximum", "Time of temperature minimum", "Number of days in zero model")
+dataFile<-c("Fn", "Tn", "Tmax", "Tmin", "pmax", "pmin", "Zero")				
 
-ab <- layout (matrix(seq(1,2), ncol=2), width=rep(lcm(wb), 2), height=rep(lcm(hb), 2))
+for(k in 1:length(dataType)){
+jpeg(paste0("c:\\Users\\hkropp\\Google Drive\\synthesis_model\\deep\\run1\\analysis_plots\\", dataFile[k],".jpg"), 
+		, width=4000,height=2000)
+
+ab <- layout (matrix(seq(1,3), ncol=3), width=rep(lcm(wb), 3), height=rep(lcm(hb), 3))
 layout.show(ab)
 
-plot(c(0,1), c(0,1),  
+par(mai=c(0,0,0,0))
+plot(c(0,1), c(0,1), xlim= c(xl[k],xh[k]), ylim= c(yh,yu), xlab = " ", ylab = " ", axes=FALSE,
+		xaxs="i", yaxs="i", type="n")
+	for(i in 1:dim(BorealID)[1]){
+		points(datAll[[k]]$Mean[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]],
+		type="b",pch=19,col=BorealID$coli[i],lwd=4, lty=1,cex=6)
+		arrows(datAll[[k]]$pc2.5[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]],
+		datAll[[k]]$pc97.5[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="boreal"&datAll[[k]]$siteid==BorealID$siteid[i]&datAll[[k]]$wyear==BorealID$wyear[i]], code=0,lwd=4)
+	}			
+	
+	box(which="plot")			
+	axis(2, seq(yha, yla, by=yi),rep(" ", length(seq(yha, yla, by=yi))), cex.axis=5, las=2 )
+	mtext(paste(seq(yha, yla, by=yi)), at=seq(yha, yla, by=yi), side=2, line=3,cex=5, las=2)
+	axis(1, seq(xla[k],xha[k], by=xi[k]),rep(" ", length(seq(xla[k],xha[k], by=xi[k]))), cex.axis=5)
+	mtext(paste(round(seq(xla[k],xha[k], by=xi[k]),2)),at=seq(xla[k],xha[k], by=xi[k]),side=1, line=5, cex=5)
+	
+	legend(xl1[k],yl1[k], c(paste(colVCT$CName[colVCT$Vclass==9]),paste(colVCT$CName[colVCT$Vclass==7])), 
+				col=c(colVCT$coli[colVCT$Vclass==9],colVCT$coli[colVCT$Vclass==7]), pch=19, lwd=4, cex=5, bty="n")
+	mtext("Boreal", side=3, cex=7, line=3)
+	mtext(paste(dataType[k]), side=1, outer=TRUE, line=-8, cex=7)
+	mtext("Depth (cm)", side=2, outer=TRUE, cex=7, line=-19)
+par(mai=c(0,0,0,0))	
+plot(c(0,1), c(0,1), xlim= c(xl[k],xh[k]), ylim= c(yh,yu), xlab = " ", ylab = " ", axes=FALSE,
+		xaxs="i", yaxs="i", type="n")
+	for(i in 1:dim(sTundraID)[1]){
+		points(datAll[[k]]$Mean[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],
+		type="b",pch=19,col=sTundraID$coli[i], cex=6, lwd=4, lty=1)
+		arrows(datAll[[k]]$pc2.5[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],
+		datAll[[k]]$pc97.5[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==sTundraID$siteid[i]&datAll[[k]]$wyear==sTundraID$wyear[i]],code=0, lwd=4)
+	
+	}	
+	box(which="plot")			
 
+	axis(1, seq(xla[k],xha[k], by=xi[k]),rep(" ", length(seq(xla[k],xha[k], by=xi[k]))), cex.axis=5)
+	mtext(paste(round(seq(xla[k],xha[k], by=xi[k]),2)),at=seq(xla[k],xha[k], by=xi[k]),side=1, line=5, cex=5)
+		legend(xl1[k],yl1[k], c(paste(colVCT$CName[colVCT$Vclass==5]),paste(colVCT$CName[colVCT$Vclass==4])), 
+				col=c(colVCT$coli[colVCT$Vclass==5],colVCT$coli[colVCT$Vclass==4]), pch=19, lwd=2, cex=5, bty="n")
+	mtext("Shrub tundra", side=3, cex=7, line=3)
+par(mai=c(0,0,0,0))	
+plot(c(0,1), c(0,1), xlim= c(xl[k],xh[k]), ylim= c(yh,yu), xlab = " ", ylab = " ", axes=FALSE,
+		xaxs="i", yaxs="i", type="n")
+	for(i in 1:dim(gTundraID)[1]){
+		points(datAll[[k]]$Mean[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],
+		type="b",pch=19,col=gTundraID$coli[i], cex=6, lwd=4, lty=1)
+		arrows(datAll[[k]]$pc2.5[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],
+		datAll[[k]]$pc97.5[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],
+		datAll[[k]]$depth[datAll[[k]]$biome=="tundra"&datAll[[k]]$siteid==gTundraID$siteid[i]&datAll[[k]]$wyear==gTundraID$wyear[i]],code=0, lwd=4
+		)
+	}	
+	box(which="plot")			
+	axis(4, seq(yha, yla, by=yi),rep(" ", length(seq(yha, yla, by=yi))), cex.axis=5, las=2 )
+	mtext(paste(seq(yha, yla, by=yi)), at=seq(yha, yla, by=yi), side=4, line=3,cex=5, las=2)
+	axis(1, seq(xla[k],xha[k], by=xi[k]),rep(" ", length(seq(xla[k],xha[k], by=xi[k]))), cex.axis=5)
+	mtext(paste(round(seq(xla[k],xha[k], by=xi[k]),2)),at=seq(xla[k],xha[k], by=xi[k]),side=1, line=5, cex=5)	
+	legend(xl1[k],yl1[k], c(paste(colVCT$CName[colVCT$Vclass==1]),paste(colVCT$CName[colVCT$Vclass==2]),paste(colVCT$CName[colVCT$Vclass==3])), 
+				col=c(colVCT$coli[colVCT$Vclass==1],colVCT$coli[colVCT$Vclass==2],colVCT$coli[colVCT$Vclass==3]),
+				pch=19, lwd=2, cex=5, bty="n")
+	mtext("Gramminoid tundra", side=3, cex=7, line=3)
+dev.off()
+	}	
