b4c50adc183c072d2d9b28f1fc7d06bd11633bdb
kroppheather <kroppheather@gmail.com>
2018-03-01 17:26:12 -0500

    initial look at soil characterstics

--- Analyses/shallow_soil/shallow_soil_comp.r
+++ Analyses/shallow_soil/shallow_soil_comp.r
@@ -21,3 +21,79 @@
 
 #run script that processes model output and puts it into organized dataframes
 source("c:\\Users\\hkropp\\Documents\\GitHub\\synthesis_database\\Analyses\\temp_parm_extract.r")
+
+#read in vege class data: check that patterns don't vary between vege type
+datV <- read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_5\\vege_class.csv")
+datVI <- read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_5\\vegeID.csv")
+#set up a plot directory
+plotDI <- "c:\\Users\\hkropp\\Google Drive\\synthesis_model\\shallow_soil\\comp"
+
+#join vege class to soilParm
+SoilParm <- join(SoilParm,datV, by=c("siteid"), type="left")
+unique(soilParm$vegeclass)
+
+
+#pull out each soil parm dataset:
+parmV <- unique(SoilParm$parm)
+
+SoilL <- list()
+
+for(i in 1:length(parmV)){
+	SoilL[[i]] <- SoilParm[SoilParm$parm==parmV[i],]
+
+}
+
+#plot all soil parms for correlation
+compI <- data.frame(c1 = c(1,1,1,1,1,2,2,2,2,3,3,3,4,4,5),
+					c2 = c(2,3,4,5,6,3,4,5,6,4,5,6,5,6,6))
+fit <- list()
+#doing a lot of comparisions, lower p value with bonfernoi cor
+bc <- 0.05/(dim(compI)[1]-1)
+
+for(i in 1:dim(compI)[1]){
+	fit[[i]] <- lm(SoilL[[compI$c2[i]]]$Mean~SoilL[[compI$c1[i]]]$Mean)
+
+	jpeg(paste0(plotDI,"\\",parmV[compI$c1[i]],"_vs_",parmV[compI$c2[i]],".jpg"),  width=1000, height=1000, units="px", quality=100)
+		par(mai=c(2,2,2,2))
+		plot(SoilL[[compI$c1[i]]]$Mean,SoilL[[compI$c2[i]]]$Mean, xlab= paste(parmV[compI$c1[i]]),ylab=paste(parmV[compI$c2[i]]), cex=2, pch=19)
+		if(summary(fit[[i]])$coefficients[2,4]<= bc){
+			abline(fit[[i]], lwd=2, col="red")
+		}else{
+			abline(h=summary(fit[[i]])$coefficients[1,1], lwd=2, col="red", lty=3)
+		}
+		
+		mtext(paste("R2 =",round(summary(fit[[i]])$r.squared,3)), side=3, line=3, cex=2) 
+		mtext(paste(parmV[compI$c2[i]],"=",round(summary(fit[[i]])$coefficients[1,1],3), " + ", round(summary(fit[[i]])$coefficients[2,1],3),"x",parmV[compI$c1[i]]), side=3, line=1, cex=2)
+		
+		
+	dev.off()
+
+}
+
+wd <- 22
+hd <-22
+
+fitV <- list()
+for(i in 1:dim(compI)[1]){
+	jpeg(paste0(plotDI,"\\vege_comp\\",parmV[compI$c1[i]],"_vs_",parmV[compI$c2[i]],".jpg"),  width=2000, height=2000, units="px", quality=100)
+	layout(matrix(seq(1,9), ncol=3, byrow=TRUE), width=rep(lcm(wd), 9), height=rep(lcm(hd),9))
+	for(j in 1:dim(datVI)[1]){
+		fitV <- lm(SoilL[[compI$c2[i]]]$Mean[SoilL[[compI$c2[i]]]$vegeclass==j]~SoilL[[compI$c1[i]]]$Mean[SoilL[[compI$c1[i]]]$vegeclass==j])
+		par(mai=c(1,1,1,1))
+		plot(SoilL[[compI$c1[i]]]$Mean[SoilL[[compI$c1[i]]]$vegeclass==j],
+			SoilL[[compI$c2[i]]]$Mean[SoilL[[compI$c2[i]]]$vegeclass==j],
+			xlab= paste(parmV[compI$c1[i]]),ylab=paste(parmV[compI$c2[i]]), cex=3,cex.axis=3,cex.lab=3, pch=19)
+	
+		if(summary(fitV)$coefficients[2,4]<= bc){
+			abline(fitV, lwd=2, col="red")
+		}else{
+			abline(h=summary(fitV)$coefficients[1,1], lwd=2, col="red", lty=3)
+		}
+		mtext(paste("p=",round(summary(fitV)$coefficients[2,4],3)), side=3, line=8, cex=2) 
+		mtext(paste("R2 =",round(summary(fitV)$r.squared,3)), side=3, line=6, cex=2) 
+		mtext(paste(parmV[compI$c2[i]],"=",round(summary(fitV)$coefficients[1,1],3), " + ", round(summary(fitV)$coefficients[2,1],3),"x",parmV[compI$c1[i]]), side=3, line=1, cex=2)
+		mtext(paste(datVI$vegename[j]), side=1, line=4, cex=2)
+	}
+
+	dev.off()
+}	
