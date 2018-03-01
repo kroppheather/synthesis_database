f9667c5e22153db366beb44d0c22479de5f8a503
kroppheather <kroppheather@gmail.com>
2018-03-01 17:37:52 -0500

    looked at air temperature data

--- Analyses/shallow_soil/shallow_soil_comp.r
+++ Analyses/shallow_soil/shallow_soil_comp.r
@@ -31,10 +31,11 @@ plotDI <- "c:\\Users\\hkropp\\Google Drive\\synthesis_model\\shallow_soil\\comp"
 #join vege class to soilParm
 SoilParm <- join(SoilParm,datV, by=c("siteid"), type="left")
 unique(soilParm$vegeclass)
-
+AirParm <- join(AirParm, datV, by=c("siteid"), type="left")
 
 #pull out each soil parm dataset:
 parmV <- unique(SoilParm$parm)
+parmA <- unique(AirParm$parm)
 
 SoilL <- list()
 
@@ -89,11 +90,48 @@ for(i in 1:dim(compI)[1]){
 		}else{
 			abline(h=summary(fitV)$coefficients[1,1], lwd=2, col="red", lty=3)
 		}
-		mtext(paste("p=",round(summary(fitV)$coefficients[2,4],3)), side=3, line=8, cex=2) 
-		mtext(paste("R2 =",round(summary(fitV)$r.squared,3)), side=3, line=6, cex=2) 
+		mtext(paste("p=",round(summary(fitV)$coefficients[2,4],3)), side=3, line=6, cex=2) 
+		mtext(paste("R2 =",round(summary(fitV)$r.squared,3)), side=3, line=3, cex=2) 
 		mtext(paste(parmV[compI$c2[i]],"=",round(summary(fitV)$coefficients[1,1],3), " + ", round(summary(fitV)$coefficients[2,1],3),"x",parmV[compI$c1[i]]), side=3, line=1, cex=2)
-		mtext(paste(datVI$vegename[j]), side=1, line=4, cex=2)
+		mtext(paste(datVI$vegename[j]), side=1, line=5, cex=2)
 	}
 
 	dev.off()
 }	
+
+
+AirL <- list()
+#see how air temp measures correlate
+for(i in 1:length(parmA)){
+	AirL[[i]] <- AirParm[AirParm$parm==parmA[i],]
+
+}
+
+#plot all soil parms for correlation
+compA <- data.frame(c1 = c(1,1,1,1,2,2,2,3,3,4),
+					c2 = c(2,3,4,5,3,4,5,4,5,5))
+					
+					
+fitA <- list()
+#doing a lot of comparisions, lower p value with bonfernoi cor
+bcA <- 0.05/(dim(compA)[1]-1)
+
+for(i in 1:dim(compA)[1]){
+	fitA[[i]] <- lm(AirL[[compA$c2[i]]]$Mean~AirL[[compA$c1[i]]]$Mean)
+
+	jpeg(paste0(plotDI,"\\Air_comp\\",parmA[compA$c1[i]],"_vs_",parmA[compA$c2[i]],".jpg"),  width=1000, height=1000, units="px", quality=100)
+		par(mai=c(2,2,2,2))
+		plot(AirL[[compA$c1[i]]]$Mean,AirL[[compA$c2[i]]]$Mean, xlab= paste(parmA[compA$c1[i]]),ylab=paste(parmA[compA$c2[i]]), cex=2, pch=19)
+		if(summary(fitA[[i]])$coefficients[2,4]<= bcA){
+			abline(fitA[[i]], lwd=2, col="red")
+		}else{
+			abline(h=summary(fitA[[i]])$coefficients[1,1], lwd=2, col="red", lty=3)
+		}
+		
+		mtext(paste("R2 =",round(summary(fitA[[i]])$r.squared,3)), side=3, line=3, cex=2) 
+		mtext(paste(parmA[compA$c2[i]],"=",round(summary(fitA[[i]])$coefficients[1,1],3), " + ", round(summary(fitA[[i]])$coefficients[2,1],3),"x",parmA[compA$c1[i]]), side=3, line=1, cex=2)
+		
+		
+	dev.off()
+
+}					
