a0da26b3314c96136008e3d062aea001a45d3fdb
kroppheather <kroppheather@gmail.com>
2018-09-25 15:13:47 -0400

    was going to look at how variable multiple regressions were by site but only about 15 sites have 3 years of data or more at 3 or more depths

---
+++ Analyses/site/site_script.r
@@ -0,0 +1,128 @@
+##########################################################
+########Vegetation site var soil temp          ###########
+########Heather Kropp started Sept 2018        ###########
+##########################################################
+##########################################################
+##########################################################
+### This script uses vegetation classes updated by     ###
+### coauthors to look at patterns in                   ###
+### air and shallow soil temperature coupling          ###
+### focus only on key soil variables: min, max, and    ###
+### the timing of the minimum                          ###
+##########################################################
+##########################################################
+### Inputs: model  output using temp_parm_extract.r    ### 
+### dataframes are Nfactor, SoilParm, AirParm          ###
+### AirRepID,SoilRepID, datCSM(list), datCAM (list)    ###
+### datAM: air temperature, datSM soil temperature     ###
+### datAT: site depth id, datAT: site height ID        ###
+### datNI: n factor ids, datAI: site year height ID    ###
+### datSI: site depth year ID                          ###
+##########################################################
+##########################################################
+
+
+#######################################
+#####read in data                 ##### 
+#######################################
+
+                    
+#run script that processes model output and puts it into organized dataframes
+source("c:\\Users\\hkropp\\Documents\\GitHub\\synthesis_database\\Analyses\\temp_parm_extract.r")
+
+#read in vege class data: check that patterns don't vary between vege type
+datV <- read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_6\\vege_class.csv")
+datVI <- read.csv("c:\\Users\\hkropp\\Google Drive\\raw_data\\backup_6\\vegeID.csv")
+
+
+#######################################
+#####libraries                    ##### 
+#######################################
+library(rjags)
+library(coda)
+library(mcmcplots)
+library(plyr)
+
+
+#######################################
+#####set directories              ##### 
+#######################################
+
+#set up a plot directory
+plotDI <- "c:\\Users\\hkropp\\Google Drive\\synthesis_model\\analyses\\site\\plots\\model"
+#model directory
+modDI <- "c:\\Users\\hkropp\\Google Drive\\synthesis_model\\analyses\\site\\model\\run1"
+Nrun <- 1
+#indicate if a model run is occuring
+modRun <- 1
+
+
+#######################################
+#####organize data                ##### 
+#######################################
+
+
+#join vege class to soilParm
+SoilParm <- join(SoilParm,datV, by=c("siteid"), type="left")
+unique(SoilParm$vegeclass)
+
+
+#create unique names for air
+colnames(AirParm)[1:4] <- paste0("A",colnames(AirParm)[1:4])
+colnames(AirParm)[8] <- paste0("A",colnames(AirParm)[8])
+#pull out each soil parm dataset:
+parmV <- unique(SoilParm$parm)
+parmA <- unique(AirParm$Aparm)
+
+#pull out relevant parameters
+#for analysis: TminS, TmaxS, peakWS
+#and subset to relevant depths
+
+parmVs <- c("TminS","TmaxS", "peakWS") 
+
+parmAs <- c("TminA","TmaxA", "peakWA") 
+
+
+SoilL <- list()
+SoilMs <- numeric(0)
+for(i in 1:length(parmVs)){
+	SoilL[[i]] <- SoilParm[SoilParm$parm==parmVs[i]&SoilParm$depth<=20,]
+	#calculate mean
+	SoilMs[i] <- round(mean(SoilL[[i]]$Mean),3)
+	#add a regression ID
+	SoilL[[i]]$regID <- rep(i,dim(SoilL[[i]])[1])
+}
+
+AirL <- list()
+AirMs <- numeric(0)
+for(i in 1:length(parmAs)){
+	AirL[[i]] <- AirParm[AirParm$Aparm==parmAs[i],]
+	#calculate mean
+	AirMs[i] <- round(mean(AirL[[i]]$AMean),3)
+	#add a regression ID
+	AirL[[i]]$regID <- rep(i,dim(AirL[[i]])[1])
+}
+
+
+#turn back into a data frame
+
+SoilR <- ldply(SoilL,data.frame)
+AirR <- ldply(AirL,data.frame)
+
+#now join soil and air DF
+ParmAll <- join(SoilR,AirR, by=c("siteid","wyear","regID"),type="left")
+
+#get the unique number of depths and years at a site
+
+SiteDepthAll <- unique(data.frame(siteid=ParmAll$siteid,depth=ParmAll$depth))
+SiteDepth <- aggregate(SiteDepthAll$depth, by=list(SiteDepthAll$siteid),FUN="length")
+colnames(SiteDepth) <- c("siteid","Ndepth")
+SiteYearAll <- unique(data.frame(siteid=ParmAll$siteid,wyear=ParmAll$wyear))
+SiteYear <- aggregate(SiteYearAll$wyear, by=list(SiteYearAll$siteid),FUN="length")
+colnames(SiteYear) <- c("siteid","NYear")
+
+SiteCount <- join(SiteYear,SiteDepth, by="siteid",type="inner")
+
+SiteCount <- join(SiteCount,datV,by="siteid",type="left")
+
+SiteCount[SiteCount$Ndepth>=3&SiteCount$NYear>=3,]
