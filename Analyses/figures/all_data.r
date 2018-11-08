b14c665acdac50bb4be15af7cf1eb397e64826b4
kroppheather <kroppheather@gmail.com>
2018-11-07 20:16:27 -0500

    made individual plots of all four variables

--- Analyses/figures/all_data.r
+++ Analyses/figures/all_data.r
@@ -99,9 +99,10 @@ vegeDepth <- unique(data.frame(depthID=countAll$depthID,vegeclass=countAll$vegec
 vegeDepth$vdID <- seq(1,dim(vegeDepth)[1])
 
 countAll <- list()
-
+maxC <- numeric(0)
 for(i in 1:dim(vegeDepth)[1]){
 	countAll[[i]] <- hist(datSTn$wdoy[datSTn$vdID==i], breaks=seq(0,367),freq=FALSE)
+	maxC[i] <- max(countAll[[i]]$counts)
 }
 
 
@@ -112,35 +113,64 @@ for(i in 1:dim(vegeDepth)[1]){
 datSTn <- join(datSTn, vegeDepth, by=c("depthID","vegeclass"), type="left")
 
 tempHist <- list()
-
+maxT <- numeric(0)
 for(i in 1:dim(vegeDepth)[1]){
 	tempHist[[i]] <- hist(datSTn$soil_t[datSTn$vdID==i], breaks=seq(-41,35),freq=FALSE)
+	maxT[i] <- max(tempHist[[i]]$density)
 }
 
-
-
+#get the maximum for each vegetation type
+maxTdf <- aggregate(maxT,by=list(vegeDepth$vegeclass),FUN="max")
+colnames(maxTdf) <- c("vegeclass","maxT")
 #try to represent all four pieces of information nicely
 test <- c(rgb(213/255,94/255,0/255,.1),rgb(240/255,228/255,66/255,.1),
 			rgb(0/255,114/255,178/255,.1),rgb(0/255,158/255,115/255,.1))
 test2 <- c(rgb(213/255,94/255,0/255,.8),rgb(240/255,228/255,66/255,.8),
-			rgb(0/255,114/255,178/255,.8),rgb(0/255,158/255,115/255,.8))			
+			rgb(0/255,114/255,178/255,.8),rgb(0/255,158/255,115/255,.8))	
+#names
+name2 <- c("herb barren", "graminoid tundra","tussock tundra","short shrub tundra","tall shrub tundra",
+					"wetland","evergreen needleleaf boreal","deciduous needleleaf boreal","mixed boreal")			
 			
 wd1 <- 40
 hd1 <- 40
 wd2 <- 20
 hd2 <- 20
-Cyl <- 	c(500,500,500,500,500,500,500,500,500)	
-Tyl <- c(500,7000,2000,2000,2000,2000,2000,2000,2000)
+Cyl <- 	round_any(max(maxC),50,ceiling)
+#seq for x2
+xh2 <- round_any(maxTdf$maxT,.05,ceiling)
+xl2 <- 0
+xi2 <- 0.05
 #line width hist
 llw <- 5
+#x axis days
+xl1 <- 0
+xh1 <- 370
+xi1 <- 50
+#seq for y axis labels
+yseq <- seq(-35,25,by=10)
+#seq for couns
+yseq2 <- seq(0,Cyl,by=50)
+
+#tick width
+lwt <- 4
+#line axis 
+alh <- 6
+#cex axis
+mx <- 4
+#box line width
+blw <- 2
+#label line
+llh <- 15
+#label size
+lx <- 6
 			
 for(i in 1:9){
-	jpeg(paste0(plotDI,"\\individual2d_class",i,".jpg"),width=1800,height=1800,quality=100)
+	jpeg(paste0(plotDI,"\\individual2d_class",i,".jpg"),width=2400,height=2400,quality=100)
 	layout(matrix(c(1,2,3,4), ncol=2,nrow=2, byrow=TRUE), widths=c(lcm(wd1),lcm(wd2)),
 			heights=c(lcm(hd2),lcm(hd1)))
 	##daily sample size##		
 		par(mai=c(0,0,0,0))
-			plot(c(0,1),c(0,1), xlim=c(0,370), ylim=c(0,Cyl[i]),type="n",xlab= " ", ylab=" ",axes=FALSE,
+			plot(c(0,1),c(0,1), xlim=c(xl1,xh1), ylim=c(0,Cyl),type="n",xlab= " ", ylab=" ",axes=FALSE,
 				xaxs="i",yaxs="i")
 			if(length(which(vegeDepth$vegeclass==i&vegeDepth$depthID==1))!=0){	
 	
@@ -166,15 +196,19 @@ for(i in 1:9){
 					countAll[[which(vegeDepth$vegeclass==i&vegeDepth$depthID==4)]]$counts,
 					col=test2[4], type="l",lwd=llw)
 			}	
+		axis(2, yseq2, rep(" ", length(yseq2)),lwd.ticks=lwt)
+		mtext(yseq2,at=yseq2,side=2,line=alh,cex=mx,las=2)
+		mtext("Measurement count", side=2,line=llh, cex=lx) 
+		mtext(paste(name2[i]), at=300, side=3, line=llh, cex=lx) 
 	##empty##		
 		par(mai=c(0,0,0,0))
-			plot(c(0,1),c(0,1), xlim=c(0,1), ylim=c(0,1),type="n",xlab= " ", ylab=" ",axes=FALSE,
+			plot(c(0,1),c(0,1), xlim=c(0,1), ylim=c(0,Tyl[i]),type="n",xlab= " ", ylab=" ",axes=FALSE,
 				xaxs="i",yaxs="i")	
 	##temperature##		
 		par(mai=c(0,0,0,0))				
 		plot(Soil[[listV[[i]][1]]]$wdoy,Soil[[listV[[i]][1]]]$soil_t,
 				type="l", ylim=c(-41,35),xlim=c(0,370),col="white",
-				xlab="day of year",ylab="temperature",xaxs="i",yaxs="i")
+				xlab=" ",ylab=" ",xaxs="i",yaxs="i",axes=FALSE)
 		for(j in 1:length(listV[[i]])){		
 			if(datI$st_depth[listV[[i]][j]]<=5){
 				points(Soil[[listV[[i]][j]]]$wdoy,
@@ -196,10 +230,17 @@ for(i in 1:9){
 					Soil[[listV[[i]][j]]]$soil_t,type="l",
 				col=test[4],lwd=3)	
 				}	
-		}	
+		}
+		axis(1, seq(xl1,xh1,xi1),rep(" ",length(seq(xl1,xh1,xi1))),lwd.ticks=lwt)
+		mtext(seq(xl1,xh1,xi1),at=seq(xl1,xh1,xi1),side=1,line=alh,cex=mx)	
+		axis(2, yseq, rep(" ", length(yseq)),lwd.ticks=lwt)
+		mtext(yseq,at=yseq,side=2,line=alh,cex=mx,las=2)
+		box(which="plot",lwd=blw)
+		mtext("Soil temperature (C)", side=2,line=llh, cex=lx) 
+		mtext("Day of water year", side=1,line=llh, cex=lx) 
 	##temperature histogram##
 		par(mai=c(0,0,0,0))
-			plot(c(0,1),c(0,1), ylim=c(-41,35), xlim=c(0,1),type="n",xlab= " ", ylab=" ",axes=FALSE,
+			plot(c(0,1),c(0,1), ylim=c(-41,35), xlim=c(xl2,xh2[i]),type="n",xlab= " ", ylab=" ",axes=FALSE,
 				xaxs="i",yaxs="i")
 			if(length(which(vegeDepth$vegeclass==i&vegeDepth$depthID==1))!=0){	
 				
@@ -229,6 +270,9 @@ for(i in 1:9){
 					
 					col=test2[4], type="l",lwd=llw)
 			}	
+		axis(1, seq(xl2,xh2[i],xi2),rep(" ",length(seq(xl2,xh2[i],xi2))),lwd.ticks=lwt)
+		mtext(seq(xl2+xi2,xh2[i],xi2),at=seq(xl2+xi2,xh2[i],xi2),side=1,line=alh,cex=mx)
+		mtext("Density", side=1,line=llh, cex=lx) 		
 	dev.off()	
 }	
 
