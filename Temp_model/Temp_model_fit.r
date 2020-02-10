399170b2c59da4260dd3b01378ab0b2874c5f2c0
kroppheather <kroppheather@gmail.com>
2020-02-10 16:45:45 -0500

    added goodness of fit plot for temperature mod

--- Temp_model/Temp_model_fit.r
+++ Temp_model/Temp_model_fit.r
@@ -68,4 +68,59 @@ fitA <- lm(repCompA$Mean~repCompA$MeasT)
 
 summary(fitA)
 abline(0,1,col="red")
-abline(fitS,col="cornflowerblue")
+abline(fitA,col="cornflowerblue")
+
+
+
+#make panel
+
+plotDI <- "c:\\Users\\hkropp\\Google Drive\\synthesis_model\\figures"	
+
+hd <- 30
+wd <- 30
+#soil range
+tl1 <- -35
+th1 <- 29
+#air range
+tl2 <- -47
+th2 <- 28
+
+lws <- 4
+sseq <- seq(-35,25, by=10)
+aseq <- seq(-50,20, by=10)
+
+png(paste0(plotDI,"\\supp_temperature_fit.png"), width=2000,height=1000,
+			units="px")
+	layout(matrix(c(1,2),ncol=2,byrow=TRUE), width=c(lcm(wd),lcm(wd)),height=lcm(hd))
+	par(mai=c(1,1,1,1))
+		
+			plot(c(0,1),c(0,1), ylim=c(tl1,th1), xlim=c(tl1,th1),
+				xlab="Observed soil temperature (C) ", 
+				ylab="Predicted soil temperature (C) ",xaxs="i",yaxs="i",axes=FALSE, cex.lab=2.5)
+			points(repCompS$MeasT,repCompS$Mean, pch=19, col=rgb(.5,.5,.5,.15), cex=1.5	)
+	text(5,-25, paste("R squared =", round(summary(fitS)$r.squared,2)), cex=2)
+	text(5,-30, paste("Predicted =", round(summary(fitS)$coefficients[1,1],2),
+		"+",round(summary(fitS)$coefficients[2,1],2),"observed"), cex=2)
+	
+	abline(0,1,col="red", lwd=lws)
+	abline(fitS,col="cornflowerblue", lty=3, lwd= lws)
+	axis(1, sseq, cex.axis=2)
+	axis(2, sseq, cex.axis=2, las=2)
+
+	par(mai=c(1,1,1,1))
+		
+			plot(c(0,1),c(0,1), ylim=c(tl2,th2), xlim=c(tl2,th2),
+				xlab="Observed air temperature (C) ", 
+				ylab="Predicted air temperature (C) ",xaxs="i",yaxs="i",axes=FALSE, cex.lab=2.5)	
+				points(repCompA$MeasT,repCompA$Mean, pch=19, col=rgb(.5,.5,.5,.15), cex=1.5	)
+	abline(0,1,col="red", lwd= lws)
+	abline(fitA,col="cornflowerblue",lty=3, lwd= lws)
+	axis(1, aseq, cex.axis=2)
+	axis(2, aseq, cex.axis=2, las=2)
+	legend("topleft", c("1:1 line", "goodness of fit"), col=c("red","cornflowerblue"),
+			lty=c(1,3), lwd=lws, bty="n", cex=2)
+	text(5,-35, paste("R squared =", round(summary(fitA)$r.squared,2)), cex=2)
+	text(5,-40, paste("Predicted =", round(summary(fitA)$coefficients[1,1],2),
+		"+",round(summary(fitA)$coefficients[2,1],2),"observed"), cex=2)		
+dev.off()	
+
